8fa1b6d2ee998e084605f59ab8f15839d11c624f
Kanyaton Somjit <113116925+Kanyatonsita@users.noreply.github.com>
2023-04-20 15:15:40 +0200

    Initial Commit

---
+++ HabitTrackerApp/HabitTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerAppApp.swift
+//  HabitTrackerApp
+//
+//  Created by Kanyaton Somjit on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
